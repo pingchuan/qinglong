965c9fe594d76a270a2b0cc924b7318b42d26334
mapingchuan <mapingchuan@xiaoduotech.com>
2020-10-17 20:04:21 +0800

    feat: eslint校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,25 @@
 module.exports = {
-  extends: "eslint:recommended",
-  rules: {
-    semi: ['error', "always"],
-    quotes: ["error", "double"],
-  },
-}
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};

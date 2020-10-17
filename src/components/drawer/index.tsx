ca632c1fdcf8aed7e28774f9bd6ab6e90d5f993f
mapingchuan <mapingchuan@xiaoduotech.com>
2020-10-17 17:47:31 +0800

    feat: drawer弹窗封装

---
+++ src/components/drawer/index.tsx
@@ -0,0 +1,82 @@
+import React, { FC } from 'react';
+import { Drawer, Button } from 'antd';
+import styles from './index.less';
+
+declare const ButtonTypes: ["default", "primary", "ghost", "dashed", "link", "text"];
+type ButtonType = typeof ButtonTypes[number];
+
+interface Props {
+  visible: boolean;
+  title?: string | JSX.Element;
+  width?: number;
+  placement?: 'right' | 'left';
+  footer?: JSX.Element;
+  footerExtend?: JSX.Element;
+  okOption?: {
+    type?: ButtonType;
+    text?: string;
+    danger?: boolean;
+  };
+  onOk?: () => void;
+  cancelOption?: {
+    type?: ButtonType;
+    text?: string;
+    danger?: boolean;
+  }
+  onCancel?: () => void;
+}
+
+const Index: FC<Props> = (props) => {
+  const {
+    children,
+    visible,
+    title,
+    width,
+    placement,
+    footer,
+    footerExtend,
+    okOption,
+    cancelOption,
+    onOk,
+    onCancel,
+  } = props;
+  const Footer = () => {
+    return (
+      <div
+        className={styles.footer}
+      >
+        <div className={styles.footerExtend}>{footerExtend}</div>
+        <div>
+          <Button
+            danger={cancelOption?.danger}
+            type={cancelOption?.type}
+            className={styles.cancelBt}
+            onClick={onCancel}
+          >{cancelOption?.text || '取消'}</Button>
+          <Button
+            danger={okOption?.danger}
+            type={okOption?.type || 'primary'}
+            onClick={onOk}
+          >{okOption?.text || '确定'}</Button>
+        </div>
+      </div >
+    );
+  }
+  return (
+    <Drawer
+      visible={visible}
+      title={title}
+      placement={placement || "right"}
+      width={width || 600}
+      closable={true}
+      destroyOnClose={true}
+      maskClosable={false}
+      onClose={onCancel}
+      footer={footer || <Footer />}
+    >
+      {children}
+    </Drawer>
+  );
+}
+
+export default Index;
--- src/pages/test1/index.tsx
+++ src/pages/test1/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Drawer from '@/components/drawer'
 import styles from './index.less';
 import { test } from './service';
 
 
 export default () => {
+  const [visible, setVisible] = useState(false);
   const send = async () => {
+    setVisible(true);
     const res = await test({ key1: 'value1' });
     console.log(res, '#send');
   }
@@ -12,6 +15,15 @@ export default () => {
   return (
     <div>
       <div className={styles.title} onClick={send}>test1</div>
+      <Drawer
+        title="添加问题"
+        visible={visible}
+        onCancel={setVisible.bind(null, false)}
+      >
+        1
+        <div style={{ height: '1000px', width: '400px' }}></div>
+        2
+      </Drawer>
     </div>
   );
 }

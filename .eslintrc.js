ce190a9b32d18dfb5a49df6270da8b9a58d91d02
mapingchuan <mapingchuan@xiaoduotech.com>
2020-10-20 20:05:18 +0800

    feat: eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
         node: true,
     },
     extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
     ],
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: 12,
-        sourceType: 'module',
+        sourceType: "module",
     },
-    plugins: ['react', '@typescript-eslint'],
+    plugins: ["react", "@typescript-eslint"],
     settings: {
         react: {
             version: "detect"
@@ -26,8 +26,8 @@ module.exports = {
     rules: {
         "semi": ["error", "always"],
         "quotes": ["error", "double"],
-        'react/display-name': 0,
-        'react/prop-types': 0,
-        '@typescript-eslint/explicit-module-boundary-types': 0,
+        "react/display-name": 0,
+        "react/prop-types": 0,
+        "@typescript-eslint/explicit-module-boundary-types": 0,
     },
 };

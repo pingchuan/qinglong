cfc6be767cc01703f2bd63abf8f5097c6866886b
mapingchuan <mapingchuan@xiaoduotech.com>
2020-10-20 19:54:24 +0800

    feat: eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/display-name': 0,
-    'react/prop-types': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-  },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/display-name': 0,
+        'react/prop-types': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+    },
 };

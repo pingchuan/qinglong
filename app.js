4f77dc93dac3a26e006aa11d5b708ba1561e7fc1
mapingchuan <mapingchuan@xiaoduotech.com>
2020-11-03 20:52:32 +0800

    feat: 修改启动脚本

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const { webNodePort: port } = require('./config/public')
 const app = express();
 
 // 通常用于加载静态资源
@@ -9,6 +8,6 @@ app.use(express.static(__dirname + '/dist'));
 app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
-app.listen(port, () => {
-  console.log('node服务启动');
+app.listen(8000, () => {
+  console.log('node服务启动在8000端口');
 });
--- config/public.ts
+++ config/public.ts
@@ -1,3 +1,2 @@
 export const apiIp = 'http://148.70.228.160:8001';
 // export const apiIp = 'http://localhost:8001';
-export const webNodePort = 8000;
